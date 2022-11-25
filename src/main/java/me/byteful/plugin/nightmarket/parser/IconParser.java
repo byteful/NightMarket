c01c0e35edf2545c6300e3137fe969c469f383d7
byteful <info@byteful.me>
2022-11-25 02:02:58 -0600

    Added support for custom model data.

--- src/main/java/me/byteful/plugin/nightmarket/parser/IconParser.java
+++ src/main/java/me/byteful/plugin/nightmarket/parser/IconParser.java
@@ -3,10 +3,12 @@ package me.byteful.plugin.nightmarket.parser;
 import com.cryptomorin.xseries.SkullUtils;
 import com.cryptomorin.xseries.XMaterial;
 import com.google.common.base.Preconditions;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import redempt.redlib.RedLib;
 
 import java.util.List;
 import java.util.Objects;
@@ -21,33 +23,45 @@ public class IconParser {
     final List<String> lore = config.getStringList("lore");
     final String mat = config.getString("material");
     final String head = config.getString("player_head");
+    final Integer customModelData = config.contains("custom_model_data") ? config.getInt("custom_model_data") : null;
 
     if (mat == null && head != null) {
-      return parseHead(name, lore, head);
+      return parseHead(name, lore, head, customModelData);
     } else if (mat != null) {
-      return parseMaterial(name, lore, Objects.requireNonNull(XMaterial.matchXMaterial(mat).orElseThrow(RuntimeException::new).parseItem()));
+      final ItemStack material = XMaterial.matchXMaterial(mat).orElseThrow(RuntimeException::new).parseItem();
+      Preconditions.checkNotNull(material, "Failed to parse material: " + mat);
+      Preconditions.checkArgument(material.getType() != Material.AIR, "Material cannot be AIR!");
+      return parseMaterial(name, lore, material, customModelData);
     } else {
       throw new RuntimeException("Failed to find either material or player head for: " + config.getName());
     }
   }
 
-  private static ItemStack parseMaterial(String name, List<String> lore, ItemStack item) {
+  private static ItemStack parseMaterial(String name, List<String> lore, ItemStack item, Integer customModelData) {
     final ItemMeta meta = item.getItemMeta();
-    meta.setDisplayName(color(name));
-    meta.setLore(lore == null || lore.isEmpty() ? null : color(lore));
+    Preconditions.checkNotNull(meta, "Failed to load item's meta.");
+    applyMeta(name, lore, customModelData, meta);
     item.setItemMeta(meta);
 
     return item;
   }
 
-  private static ItemStack parseHead(String name, List<String> lore, String texture) {
+  private static ItemStack parseHead(String name, List<String> lore, String texture, Integer customModelData) {
     final ItemStack item = Objects.requireNonNull(XMaterial.PLAYER_HEAD.parseItem());
     SkullMeta meta = (SkullMeta) item.getItemMeta();
+    Preconditions.checkNotNull(meta, "Failed to load item's meta.");
     meta = SkullUtils.applySkin(meta, texture);
-    meta.setDisplayName(color(name));
-    meta.setLore(lore == null || lore.isEmpty() ? null : color(lore));
+    applyMeta(name, lore, customModelData, meta);
     item.setItemMeta(meta);
 
     return item;
+  }
+
+  private static void applyMeta(String name, List<String> lore, Integer customModelData, ItemMeta meta) {
+    meta.setDisplayName(color(name));
+    meta.setLore(lore == null || lore.isEmpty() ? null : color(lore));
+    if (RedLib.MID_VERSION >= 14) {
+      meta.setCustomModelData(customModelData);
+    }
   }
 }
